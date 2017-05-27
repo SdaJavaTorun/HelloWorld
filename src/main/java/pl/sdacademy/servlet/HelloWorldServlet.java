0a81509a4e4659460b4ce25aeaa03478792b08af
Maciej Boniewicz <amossoma@o2.pl>
2017-05-27 23:32:04 +0200

    Init config

--- src/main/java/pl/sdacademy/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sdacademy/servlet/HelloWorldServlet.java
@@ -10,6 +10,6 @@ public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello World!");
+        resp.getWriter().println(getServletConfig().getInitParameter("message"));
     }
 }
